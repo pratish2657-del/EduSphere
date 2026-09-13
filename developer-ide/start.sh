423ca6c7d35daa19c649ddefafce3b09d63eb2e7
Pratish Kumar Agarwal <pratish2657@gmail.com>
2026-09-13 19:07:36 +0530

    Fix developer IDE Docker build

--- developer-ide/start.sh
+++ developer-ide/start.sh
@@ -5,5 +5,7 @@ export PORT="${PORT:-10000}"
 export IDE_BRIDGE_PORT="${IDE_BRIDGE_PORT:-8081}"
 
 mkdir -p /home/coder/workspace
+
 envsubst '${PORT}' < /opt/ide/nginx.conf > /tmp/nginx.conf
+
 exec /usr/bin/supervisord -c /opt/ide/supervisord.conf
