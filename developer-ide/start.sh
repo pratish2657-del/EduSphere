b4aab865870ae7906f9fbf5e971527a44202c9e5
Pratish Kumar Agarwal <pratish2657@gmail.com>
2026-09-13 19:12:20 +0530

    Fix code-server user and IDE startup

--- developer-ide/start.sh
+++ developer-ide/start.sh
@@ -5,6 +5,7 @@ export PORT="${PORT:-10000}"
 export IDE_BRIDGE_PORT="${IDE_BRIDGE_PORT:-8081}"
 
 mkdir -p /home/coder/workspace
+chown -R coder:coder /home/coder/workspace
 
 envsubst '${PORT}' < /opt/ide/nginx.conf > /tmp/nginx.conf
 
